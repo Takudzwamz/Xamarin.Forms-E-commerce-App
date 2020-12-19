efcc52f4ab2b4e6825b1f7f04bfba7a1ce5eb98c
zhout <zhout@DESKTOP-FSIIDVA>
2020-12-20 00:08:11 +0200

    Add project files.

---
+++ RealWorldApp/RealWorldApp/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealWorldApp
+{
+    public static class AppSettings
+    {
+        public static string ApiUrl = "https://foodapi-fi2.conveyor.cloud/";
+       // public static string ApiUrl = "http://192.168.8.102:45455/";
+    }
+}
