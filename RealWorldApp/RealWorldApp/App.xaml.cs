671929b3ecf38b1331397f2197c2e555d6d3cc38
Takudzwa Mupanesure <zhout1709@hotmail.com>
2021-05-13 01:45:58 +0200

    UI changes

--- RealWorldApp/RealWorldApp/App.xaml.cs
+++ RealWorldApp/RealWorldApp/App.xaml.cs
@@ -14,6 +14,7 @@ namespace RealWorldApp
         {
             InitializeComponent();
 
+            Sharpnado.Shades.Initializer.Initialize(loggerEnable: false);
             DependencyService.Register<APIDataStore>();
 
             if (Preferences.ContainsKey(Constants.AccessToken))
